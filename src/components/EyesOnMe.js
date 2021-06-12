2f794f391de7d5d12ab6d532977ec03fefaaae69
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-06-12 08:41:16 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,6 +1,11 @@
 // Code EyesOnMe Component Here
 
+<<<<<<< HEAD
 import React, { Component } from 'react';
+=======
+import React, { Component } from 'react'
+
+>>>>>>> fd76ec766cb0426cdbe07919c3fe11148063a038
 
 export default class EyesOnMe extends Component {
   render() {
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -2,8 +2,19 @@
 
 import React, { Component } from 'react';
 
+<<<<<<< HEAD
 export default class Keypad extends Component {
   render() {
     return <input type="password" onKeyUp={()=>console.log("Entering password...")}/>
   }
 }
+=======
+
+function Keypad () {
+  render() {
+    return <input type="password" onKeyUp={()=>console.log("Entering password...")}/>
+  }
+}
+
+export default Keypad;
+>>>>>>> fd76ec766cb0426cdbe07919c3fe11148063a038
